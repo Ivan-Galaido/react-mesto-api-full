0acbdc6df8f213756df858b422ad945bd1a18d22
Ivan-Galaido <75268958+Ivan-Galaido@users.noreply.github.com>
2021-08-15 21:44:27 +0300

    Add files via upload

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+const UnauthorizedError = require('../errors/401-unauthorized-error');
+const ForbiddenError = require('../errors/403-forbidden-error');
+
+dotenv.config();
+const { JWT_SECRET, NODE_ENV } = process.env;
+
+exports.Auth = (req, res, next) => { 
+
+  const token = req.cookies.userToken;
+  let payload;
+
+  try {
+  // верифицируем токен
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
+  } catch (err) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+  next(); // пропускаем запрос дальше
+};
