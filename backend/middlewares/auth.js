ed21d6e03a5b20b0ec06a471b9496f97a0b70d49
Ivan-Galaido <75268958+Ivan-Galaido@users.noreply.github.com>
2021-08-18 20:19:26 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,17 +2,16 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 
 const UnauthorizedError = require('../errors/401-unauthorized-error');
-const ForbiddenError = require('../errors/403-forbidden-error');
 
 dotenv.config();
 const { JWT_SECRET, NODE_ENV } = process.env;
 
 exports.Auth = (req, res, next) => { 
 
-  const token = req.cookies.userToken;
-  let payload;
+const token = req.cookies.userToken;
+let payload;
 
-  try {
+try {
   // верифицируем токен
     payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
   } catch (err) {
